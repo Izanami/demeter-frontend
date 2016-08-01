f1fd41b169d831786d18c5cfc74404891b9a6f90
Adrien Jeser <adrien@jeser.me>
2016-08-02 01:14:04 +0200

    Devices model

--- app/routes/devices.js
+++ app/routes/devices.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
+let devices = [
+    {
+    id: 1,
+    name: 'Température',
+    lastUpdate: Date.now()
+    },
+    {
+    id: 2,
+    name: 'Humidité',
+    lastUpdate: Date.now()
+    }];
+
 export default Ember.Route.extend({
+    model() {
+        return devices;
+    }
 });
